3f1977509dc3aa6dbb51e8905f558a961b718fa3
Ginca <48185940+reactst@users.noreply.github.com>
2021-09-07 23:47:07 +0200

    v0.1
    First commit

---
+++ promgram.py
@@ -0,0 +1,45 @@
+import eyed3
+import os
+
+
+def applyMetadataToSingeFile(filename):
+    artist = filename.split("-")[0]
+    song = filename.split("-")[1][:-4]
+    audiof = eyed3.load(filename)
+    if not audiof.tag:
+        audiof.initTag()
+    audiof.tag.artist = artist
+    audiof.tag.title = song
+    audiof.tag.save()
+
+
+def applyMetadataToFolder(folderName):
+    prefix = "./"
+    folder = prefix + folderName
+    print(os.listdir(folder))
+    os.chdir(folder)
+    for i in os.listdir():
+        if i.find("-") == -1 and i.find(".mp3") == -1:
+            continue
+        artist = i.split("-")[0]
+        song = i.split("-")[1][:-4]
+        audiof = eyed3.load(i)
+        if not audiof.tag:
+            audiof.initTag()
+        audiof.tag.artist = artist
+        audiof.tag.title = song
+        audiof.tag.save()
+
+
+choice = input("Apply metadata on Folder/File: ")
+choice = choice.lower()
+if choice == "folder":
+    fldnm = input ("Enter child folder name: ")
+    applyMetadataToFolder(fldnm)
+elif choice == "file":
+    flnm = input ("Enter file name: ")
+    if flnm.find(".mp3")==-1:
+        flnm+=".mp3"
+    applyMetadataToSingeFile(flnm)
+else:
+    print ("Incorrect name")
